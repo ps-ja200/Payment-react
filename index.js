e07fd296bb2df86855a3cf189e1873f94288a5e6
ps-ja200 <119111466+ps-ja200@users.noreply.github.com>
2024-10-26 19:16:16 +0530

    Praveer Jaggi

---
+++ index.js
@@ -0,0 +1,5 @@
+import 'react-native-url-polyfill/auto';  // Ensure this line is the very first import
+import { AppRegistry } from 'react-native';
+import App from './app';
+
+AppRegistry.registerComponent('main', () => App);
